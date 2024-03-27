d554fea444cf45f0c25a61a37d1a809596cf0d25
Tejasvi Urkande <tejasviurkande@gmail.com>
2024-03-27 15:46:00 +0530

    third commit

--- 28-34 forms and UseRef/todo/src/App.jsx
+++ 28-34 forms and UseRef/todo/src/App.jsx
@@ -9,12 +9,10 @@ function App() {
   const [todoItems, setTodoItems] = useState([]);
 
   const handleNewItem = (itemName, itemDueDate) => {
-    console.log(`New Item Added: ${itemName} Date:${itemDueDate}`);
-    const newTodoItems = [
-      ...todoItems,
+    setTodoItems((currValue) => [
+      ...currValue,
       { name: itemName, dueDate: itemDueDate },
-    ];
-    setTodoItems(newTodoItems);
+    ]);
   };
 
   const handleDeleteItem = (todoItemName) => {
@@ -23,13 +21,6 @@ function App() {
   };
 
   return (
-    <TodoItemsContext.Provider
-    value={{
-      todoItems: todoItems,
-      addNewItems: addNewItem,
-      deleteItem : deleteItem,
-    }}
-    >
     <center className="todo-container">
       <AppName />
       <AddTodo onNewItem={handleNewItem} />
@@ -39,16 +30,7 @@ function App() {
         onDeleteClick={handleDeleteItem}
       ></TodoItems>
     </center>
-    </TodoItemsContext.Provider>
   );
 }
 
 export default App;
-
-
-// CONTEXT API
-// let see need of context api, in todo app, suppose we want todo item or todo item ko hume state mai rakhna hai and we replaced state by useref
-// still humne kaha tha ki, agar koi state shared hai multiple components mai toh unka jo shared parent hai waha hume state share krne padege
-// manytime , it happens that one state is wanted by many component, so wo bubble up hoke root element ke pass pohch jaate hai, so root ke pass bhot sare state jama hote hai, so he has maintain it
-// context means common shared storage amoung all component
-
